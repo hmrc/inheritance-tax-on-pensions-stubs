216102bb2998aee07e4215fac8396d6c3931a757
kberec <26416940+kberec@users.noreply.github.com>
2025-12-12 14:22:29 +0000

    [IHTP-155] Initial scoverage config

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -11,12 +11,14 @@ object CodeCoverageSettings {
     "prod.*",
     ".*Routes.*",
     "testOnly.*",
-    "testOnlyDoNotUseInAppConf.*"
+    "testOnlyDoNotUseInAppConf.*",
+    ".*repository",
+    "uk.gov.hmrc.inheritancetaxonpensionsstubs.config.AppConfig"
   )
 
   val settings: Seq[Setting[_]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
-    ScoverageKeys.coverageMinimumStmtTotal := 100,
+    ScoverageKeys.coverageMinimumStmtTotal := 95,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
